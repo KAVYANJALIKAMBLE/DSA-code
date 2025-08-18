6784d663dd18c1efb0d714f776398ceceeabf299
KAVYANJALIKAMBLE <kavyanjalikamble29@gmail.com>
2025-08-18 19:36:23 +0530

    Create day4.c
    Merge of two sorted arrays

---
+++ day4.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+
+//func to calculate next gap
+int nextGap(int gap) {
+    if (gap<=1)
+    return 0;
+    return (gap/2)+ (gap% 2);
+}
+
+//func to merge arr1[] and arr2[] in place
+void merge(int arr1[],int m, int arr2[],int n){
+    int gap = m + n;
+    for (gap=nextGap(gap);gap>0;gap=nextGap(gap)) {
+        int i,j;
+
+        //Compare elements in first array
+        for (i=0; i+ gap<m; i++){
+            if (arr1[i]>arr1[i+gap]) {
+                int temp=arr1[i];
+                arr1[i]=arr1[i+gap];
+                arr1[i + gap]=temp;
+            }
+        }
+
+        //Compare elements between both arrays
+
+        for (j=(gap>m)? gap-m: 0;i<m && j<n;i++,j++){
+            if (arr1[i]>arr2[j]){
+                int temp = arr1[i];
+                arr1[i] = arr2[j];
+                arr2[j] = temp;
+            }
+        }
+
+        //compare elements in second array
+
+        if(j<n){
+            for (j= 0; j+gap<n; j++);
+        }
+        if(arr2[j]> arr2[j+ gap])
+        {
+          int temp = arr2[j];
+          arr2[j+gap]=temp;
+    }
+}
+
+// Utility func to print array
+void printArray (int arr[],int size)
+{
+  for (int i=0;i<size;i++)
+    printf("%d",arr[i]);
+printf("/n");
+}
+int main() {
+    //Test Case Example
+    int arr1[]={1,3,5,7};
+    int arr2[]={2,4,6,8};
+  
+    int m=sizeof(arr1)/sizeof(arr1[0]);
+    int n=sizeof(arr2)/sizeof(arr2[0]);
+
+  merge(arr1,m,arr2,n);
+
+  printf("arr1: ");
+
+  printArray(arr1,m);
+
+  printf("arr2:");
+  printArray(arr2,n);
+
+  return 0;
+}
